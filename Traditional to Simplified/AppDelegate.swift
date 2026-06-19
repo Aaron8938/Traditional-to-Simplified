f7bbf2aa5805a14b9f81fba3dba8960815156c62
Aaron <893861417@qq.com>
2026-06-19 08:57:26 +0800

    Initial Commit

---
+++ Traditional to Simplified/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Traditional to Simplified
+//
+//  Created by Aaron Deng on 2026/6/19.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
